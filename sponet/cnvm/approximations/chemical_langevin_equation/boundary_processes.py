6bf6ed9d3e64c64fdede6a45a9f9b72340e6a9af
MUrban <manuel.urban@uni-bayreuth.de>
2025-09-22 08:31:01 +0200

    minor changes

--- sponet/cnvm/approximations/chemical_langevin_equation/boundary_processes.py
+++ sponet/cnvm/approximations/chemical_langevin_equation/boundary_processes.py
@@ -501,17 +501,13 @@ def _project_onto_standard_simplex(x: NDArray) -> NDArray:
 
 def _map_to_simplex_facette(x: NDArray, facette_index: int) -> NDArray:
 
-
     res = np.empty(x.shape[0] + 2)
     res[facette_index] = 0
 
     if facette_index == x.shape[0]:
-        res[0] = 1-np.sum(x)
+        res[0] = 1 - np.sum(x)
 
-
-    res[(facette_index+1) % (res.shape[0]+1)] = 1 - np.sum(x)
-
-    res[]
+    res[(facette_index + 1) % (res.shape[0] + 1)] = 1 - np.sum(x)
 
 
 def get_boundary_process_from_alias(alias: str) -> BoundaryProcess:

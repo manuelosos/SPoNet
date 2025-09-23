98c47c942f924dc6d685d5a722dcd297e4852744
MUrban <manuel.urban@uni-bayreuth.de>
2025-09-23 10:49:50 +0200

    add first version of visualize_mfe_vector_field

--- sponet/plotting.py
+++ sponet/plotting.py
@@ -1,8 +1,15 @@
 import numpy as np
 from matplotlib.axes import Axes
+from numba import njit
+
 from .collective_variables import CollectiveVariable
 from .parameters import Parameters
+from .cnvm.parameters import CNVMParameters
 from .multiprocessing import sample_many_runs
+from sponet.cnvm.approximations.chemical_langevin_equation import _drift_and_diffusion
+
+from numpy.typing import NDArray
+import matplotlib.pyplot as plt
 
 
 def plot_trajectories(
@@ -60,3 +67,131 @@ def plot_trajectories(
     ax.grid()
 
     return ax
+
+
+def visualize_mfe_vector_field(
+    params: CNVMParameters,
+    ax: Axes,
+    resolution: int = 15,
+):
+    """
+    Visualizes the vector field given by the RRE/MFE on the simplex.
+
+    Only implemented for n_opinions = 3.
+
+
+    Parameters
+    ----------
+    params: CNVMParameters
+    resolution: int
+        Determines how many points there are on a length of one unit.
+    ax: Axes
+
+    Returns
+    -------
+    Axes
+    """
+    r = params.r
+    r_tilde = params.r_tilde
+    n_states = r.shape[0]
+
+    anchor_points = simplex_grid(3, 1 / resolution)
+
+    # TODO Write fast function for isometric projection
+    trans_matrix = np.column_stack(
+        [np.array([1, -1, 0]) / np.sqrt(2), np.array([1, 1, -2]) / np.sqrt(6)]
+    )  # 3x2 matrix
+    bary = np.array([1 / 3, 1 / 3, 1 / 3])
+
+    projected_anchor_points = np.empty((anchor_points.shape[0], n_states - 1))
+    projected_anchor_vectors = np.empty_like(projected_anchor_points)
+    projected_anchor_points = (trans_matrix.T @ (anchor_points - bary).T).T
+
+    for i in range(anchor_points.shape[0]):
+        tmp, _ = _drift_and_diffusion(
+            anchor_points[i], r, r_tilde, 10
+        )  # TODO Use own MFE function
+        projected_anchor_vectors[i] = trans_matrix.T @ (tmp - bary)
+
+    ax.quiver(
+        projected_anchor_points[:, 0],
+        projected_anchor_points[:, 1],
+        projected_anchor_vectors[:, 0],
+        projected_anchor_vectors[:, 1],
+    )
+
+    # Plot triangle frame
+    unit_vectors = np.vstack((np.eye(n_states), np.eye(n_states)[0]))
+    projected_unit_vectors = (trans_matrix.T @ (unit_vectors - bary).T).T
+    ax.plot(projected_unit_vectors[:, 0], projected_unit_vectors[:, 1], zorder=-1)
+
+    ax.set_aspect("equal")
+
+    return ax
+
+
+@njit(cache=True)
+def binomial(n: int, r: int) -> int:
+    if r < 0 or r > n:
+        return 0
+    if r == 0 or r == n:
+        return 1
+    # compute combinatorially, safe for small sizes
+    rr = r if r <= n - r else n - r
+    res = 1
+    for i in range(rr):
+        res = (res * (n - i)) // (i + 1)
+    return res
+
+
+@njit(cache=True)
+def simplex_grid(dim: int, resolution: float) -> np.ndarray:
+    """
+    Uniform grid on the standard M-simplex using spacing ~ resolution.
+    Non-recursive, Numba-friendly.
+    Returns an array of shape (N, M) with rows summing to 1.
+    """
+    if dim <= 1:
+        raise ValueError("M must be > 1")
+    # convert resolution to integer denominator K (round nearest)
+
+    n_points_per_unit = max(1, int(1 / resolution + 0.5))
+
+    nSlots = n_points_per_unit + dim - 1  # total slots in stars-and-bars
+    r = dim - 1  # number of separators (bars)
+
+    N = binomial(n_points_per_unit + dim - 1, dim - 1)
+    pts = np.empty((N, dim), dtype=np.float64)
+
+    # initialize first combination s = [0,1,2,...,r-1] (zero-based positions)
+    s = np.empty(r, dtype=np.int64)
+    for i in range(r):
+        s[i] = i
+
+    idx = 0
+    while True:
+        # compute composition from separators s:
+        # n0 = s[0]
+        # ni = s[i] - s[i-1] - 1  for i=1..r-1
+        # n_{M-1} = (nSlots - 1) - s[r-1]
+        for j in range(dim):
+            if j == 0:
+                compj = s[0]
+            elif j == dim - 1:
+                compj = (nSlots - 1) - s[r - 1]
+            else:
+                compj = s[j] - s[j - 1] - 1
+            pts[idx, j] = compj / n_points_per_unit
+        idx += 1
+
+        # generate next combination in lexicographic order
+        i = r - 1
+        while i >= 0 and s[i] == (nSlots - r + i):
+            i -= 1
+        if i < 0:
+            break
+        s[i] += 1
+        for j in range(i + 1, r):
+            s[j] = s[j - 1] + 1
+
+    return pts
